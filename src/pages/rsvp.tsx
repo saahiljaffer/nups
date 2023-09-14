35b171b417433d46b0553b23a8602dcf9f15aa2b
Saahil Jaffer <contact@saahiljaffer.com>
2023-09-14 14:18:21 -0400

    add card

---
+++ src/pages/rsvp.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="wrapper h-screen overflow-hidden bg-[#8fac97]">
+        <div className="mx-auto flex flex-col items-end gap-8 p-8 first-letter:flex-col">
+          <p className="uppercase">Aamir Jaffer</p>
+          <p className="uppercase">Aamena Dharsi</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+//
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,28 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        slideOut: {
+          "0%": { left: "0" },
+          "100%": { left: "100%" },
+        },
+        appear: {
+          "0%": { opacity: "0" },
+          "90%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        slideOut: "slideOut 4s forwards",
+        appear: "appear 3s forwards",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
